5bb319ad9222963fb3a4787766ed1b52620fedfe
HRDgithub <rop.hrdassoc@gmail.com>
2024-08-09 22:20:06 +0300

    version 1.0.0

--- lib/supabase/supabase_api.dart
+++ lib/supabase/supabase_api.dart
@@ -1,5 +1,5 @@
 class SupabaseAPIs {
-  static const url = 'https://cmxguysudghegtraxteq.supabase.co';
-  static const anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNteGd1eXN1ZGdoZWd0cmF4dGVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2OTAxMTYsImV4cCI6MjAzNjI2NjExNn0.TgKdNIlJe0vB_vmRk8xfPVSu0W8JHr9mybHWy9qrl_Q';
+  //* Add Yours
+  static const url = '';
+  static const anonKey = '';
 }
